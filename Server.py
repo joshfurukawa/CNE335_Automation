97d97b195cfbfe090315952af5682c6473294354
joshfurukawa <Joshfurukawa@gmail.com>
2021-12-08 08:48:55 -0800

    abc

--- Server.py
+++ Server.py
@@ -7,4 +7,5 @@ class Server:
 
     def ping(self):
         # TODO - Use os module to ping the server
+
         return
--- main.py
+++ main.py
@@ -4,10 +4,16 @@
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Josh Furukawa")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    import os
+
+    server = "35.87.239.198"
+
+    os.system('ping -n 4 {}'.format(server))
     # TODO - Call Ping method and print the results
+    print(server.ping())
